e682dd420fcdb9be392a6c0f3afd04cd598faa74
Kritik Modi <61862301+kritikmodi@users.noreply.github.com>
2024-08-23 18:35:32 +0530

    Update main.py

--- parquet_test_batch/main.py
+++ parquet_test_batch/main.py
@@ -12,23 +12,22 @@ def __batch_main__(sub_job_name, scheduled_time, runtime, part_num, num_parts, j
     num_rows = 10**7
     num_columns = 10
     file_path = '/tmp/test_parquet.parquet'
+    print('Creating large Parquet file...')
+    data = {f'col_{i}': np.random.rand(num_rows) for i in range(num_columns)}
+    df = pd.DataFrame(data)
+    table = pa.Table.from_pandas(df)
+    pq.write_table(table, file_path)
+    print(f'Parquet file created at : {file_path}')
     duration = 7200
     interval = 120
     num_iterations = duration // interval
+    print('Starting memory operations...\n\n\n')
     for _ in range(num_iterations):
         try:
-            print('Creating large Parquet file...')
-            data = {f'col_{i}': np.random.rand(num_rows) for i in range(num_columns)}
-            df = pd.DataFrame(data)
-            table = pa.Table.from_pandas(df)
-            pq.write_table(table, file_path)
-            print(f'Parquet file created at : {file_path}')
-            parquet_file = pq.ParquetFile(file_path)
-            print(f'Opened {file_path}')
-            parquet_file = None
-            print(f'Closed {file_path}')
-            os.remove(file_path)
-            print(f'Deleted {file_path}\n')
+            parquet_file = pd.read_parquet(file_path)
+            print(f'Created parquet file reference')
+            del parquet_file
+            print(f'Deleted parquet file reference\n')
             time.sleep(interval)
         except Exception as e:
             print(f'Error occurred: {e}')
